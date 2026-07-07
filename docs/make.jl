099950b16b1c04f67a016c5329fcb5392b713a40
Rafael Baez Ramirez <rbaezra@sandia.gov>
2026-07-07 15:17:32 -0600

    Documenter changes

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,23 @@
 using Documenter
 using WaveRealizations
 
+DocMeta.setdocmeta!(SpectralSuperpositions, :DocTestSetup, :(using SpectralSuperpositions); recursive = true)
+#bib = CitationBibliography(joinpath(@__DIR__, "references.bib"))
+
 makedocs(
     sitename = "WaveRealizations.jl",
-    modules = [WaveRealizations],
-    format = Documenter.HTML(),
-    pages = [
-        "Home" => "index.md"
-    ]
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", "false") == "true",
+        assets = String["src/index.css"]
+    ),
+    modules = [SpectralSuperpositions],
+    pages = ["Home" => "index.md",
+            "API" => "api.md"]    #plugins = [bib],
 )
 
 deploydocs(
     repo = "github.com/JuliaOceanWaves/WaveRealizations.jl.git",
-    push_preview = true,
-    versions = ["latest" => "v^", "v#"])
+    versions = ["stable" => "v^", "v#"],
+    devbranch = "main",
+    push_preview = true
+)
---
+++ docs/src/api.md
@@ -0,0 +1,7 @@
+# API
+
+```@autodocs
+Modules = [
+    WaveRealizations
+    ]
+```
